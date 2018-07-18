ed318d843371cb1c8dcaaeaf036b28a2f23a20ce
samvicky <billavignesh26@gmail.com>
2018-07-18 18:49:47 +0530

    Difficulty changed LWMA-2

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -14,6 +14,9 @@
 
 namespace CryptoNote {
 namespace parameters {
+	
+const uint64_t DIFFICULTY_TARGET                             = 120; // seconds
+const uint64_t DIFFICULTY_TARGET_V2                          = 90; // seconds
 
 const uint32_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
@@ -21,13 +24,21 @@ const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
 const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x1db32;
 const uint32_t CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 10;
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
+const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V3         = 3 * DIFFICULTY_TARGET; 
 
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
+const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW_V3          = 11; 
 
 const uint64_t MONEY_SUPPLY                                  = UINT64_C(18446744073709551615);
 const uint32_t ZAWY_DIFFICULTY_BLOCK_INDEX                   = 1;
 const size_t ZAWY_DIFFICULTY_V2                              = 0;
 const uint8_t ZAWY_DIFFICULTY_DIFFICULTY_BLOCK_VERSION       = 0;
+
+const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX                 = 20; 
+
+const uint64_t DIFFICULTY_WINDOW_V3                          = 60; 
+const uint64_t DIFFICULTY_BLOCKS_COUNT_V3                    = DIFFICULTY_WINDOW_V3 + 1; 
+
 const unsigned EMISSION_SPEED_FACTOR                         = 18;
 const unsigned EMISSION_SPEED_FACTOR_V2                      = 20;
 const unsigned EMISSION_SMOOTHING_FACTOR_V2                  = 22;
@@ -49,8 +60,6 @@ const uint16_t MAXIMUM_MIXIN_V1                              = 30;
 const uint32_t MIXIN_LIMITS_V1_HEIGHT                        = 44500;
 const uint64_t DEFAULT_DUST_THRESHOLD                        = UINT64_C(100000);
 
-const uint64_t DIFFICULTY_TARGET                             = 120; // seconds
-const uint64_t DIFFICULTY_TARGET_V2                          = 90; // seconds
 const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 / DIFFICULTY_TARGET;
 const size_t   DIFFICULTY_WINDOW                             = 17;
 const size_t   DIFFICULTY_WINDOW_V1                          = 720;
