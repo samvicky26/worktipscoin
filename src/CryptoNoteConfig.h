45e49217bca849f0b42003918e327f16f4787707
Vordas <worktipscoin@gmail.com>
2018-08-14 13:35:21 +0200

    1.0.3.1 upgrade
    - removed poolwallet
    - further improved the LWMA-2
    - added exporting of private keys to GUI lite wallet

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -23,8 +23,9 @@ const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
 const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
 const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x1db32;
 const uint32_t CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 10;
-const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
-const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V3         = 3 * DIFFICULTY_TARGET; 
+const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2; 
+const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V3         = 3 * DIFFICULTY_TARGET;
+const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V4         = 6 * DIFFICULTY_TARGET_V2; 
 
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW_V3          = 11; 
@@ -35,6 +36,7 @@ const size_t ZAWY_DIFFICULTY_V2                              = 0;
 const uint8_t ZAWY_DIFFICULTY_DIFFICULTY_BLOCK_VERSION       = 0;
 
 const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX                 = 96000; 
+const uint64_t LWMA_2_DIFFICULTY_BLOCK_INDEX_V2              = 110000;
 
 const uint64_t DIFFICULTY_WINDOW_V3                          = 60; 
 const uint64_t DIFFICULTY_BLOCKS_COUNT_V3                    = DIFFICULTY_WINDOW_V3 + 1; 
